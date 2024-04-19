fb7739f1620247ce7c2abecdfb2feb8f1f4cc2ed
Maks Ziembicki <mtziembicki@gmail.com>
2024-04-20 00:26:56 +0200

    adding sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
-import { Box, useMediaQuery } from '@mui/material'
-import { UseSelector } from 'react-redux'
-import Navbar from 'components/Navbar'
-import { Outlet } from 'react-router-dom'
-
+import React from "react";
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { UseSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import { Outlet } from "react-router-dom";
+import Sidebar from "components/Sidebar";
 
 //dzieki box moge dawac cssa
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width="100%" height='100%' >
-        <Box>
-            <Navbar />
-            <Outlet />
-        </Box>
+    <Box width="100%" height="100%" display={isNonMobile ? "flex" : "block"}>
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
